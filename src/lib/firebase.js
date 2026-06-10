856fad634737ef59e65af9061d42e3706c71e9b4
Ahmm _ <mru3337@gmail.com>
2026-06-11 01:00:41 +0530

    chore: update Firebase config for atlscoup project, remove App Check

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,16 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, browserLocalPersistence, setPersistence } from 'firebase/auth';
 import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpvc3W7oPxw1RECDRmeTjyw3DuWT0goBc",
-  authDomain: "atlcoup.firebaseapp.com",
-  projectId: "atlcoup",
-  storageBucket: "atlcoup.appspot.com",
-  messagingSenderId: "420534898644",
-  appId: "1:420534898644:web:61341c070ca79043910abc",
-  measurementId: "G-492TK2FTN7"
+  apiKey: "AIzaSyBSjiUAs9DO4z7lVKdj8pnFj8pioSFsBJM",
+  authDomain: "atlscoup.firebaseapp.com",
+  projectId: "atlscoup",
+  storageBucket: "atlscoup.firebasestorage.app",
+  messagingSenderId: "451427018442",
+  appId: "1:451427018442:web:8466a4200f950b6501b633",
+  measurementId: "G-XJ6RV6PYMX"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,15 +21,5 @@ export const db = initializeFirestore(app, {
 export const storage = getStorage(app);
 
 setPersistence(auth, browserLocalPersistence);
-
-const RECAPTCHA_V3_KEY = '6LdiLRctAAAAABIZmRn1RA7vKq7a-XqvoegWAF-u';
-const isLocalhost = typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1');
-
-if (!isLocalhost && RECAPTCHA_V3_KEY) {
-  initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider(RECAPTCHA_V3_KEY),
-    isTokenAutoRefreshEnabled: true,
-  });
-}
 
 export default app;
